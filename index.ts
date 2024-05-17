319d2bd17db96e9cb34605ac9052f35e18b1c5cb
KibetBrian <briankibet2040@gmail.com>
2024-05-17 17:31:00 +0300

    template

--- index.ts
+++ index.ts
@@ -1,14 +1,35 @@
 import express, { Response } from 'express';
+import cors from 'cors';
+import hpp from 'hpp';
+import { StatusCodes } from 'http-status-codes';
 
 const app = express();
+app.use(express.json());
+
+const corsOrigin = [process.env.CORS_ORIGIN ?? 'http://localhost:3000', 'https://lemonsqueezy.com/'];
+
+const corsOptions = {
+  origin: corsOrigin,
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(hpp());
+
+// Allow prisma to serialize bigint
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(BigInt.prototype as any).toJSON = function () {
+  return Number(this);
+};
 
 const port = 8080;
 
-app.get('/', (_, res: Response) => {
-  res.send('Ok');
+app.get('/health', (req, res: Response) => {
+  res.status(StatusCodes.OK).send('OK');
 });
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${  port}`);
+  console.log(`Server is running on port ${port}`);
 });
